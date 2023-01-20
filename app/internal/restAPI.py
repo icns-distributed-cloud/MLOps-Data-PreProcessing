b11bc0876483a6d06d27848a7fcf4b342269e141
inhun <torihome1@naver.com>
2023-01-20 18:58:15 +0900

    fix

--- app/internal/restAPI.py
+++ app/internal/restAPI.py
@@ -4,12 +4,12 @@ import httpx
 
 
 def update_pre_status(db_id, path, filename, preProcessState=1):
-    response = httpx.post(f'http://163.180.117.186:18088/api/predataset/post/updateprestatus', json={'preDatasetId': db_id, 'path': path, 'fileName': filename, 'preProcessState': preProcessState})
+    response = httpx.post(f'http://back.api:18088/api/predataset/post/updateprestatus', json={'preDatasetId': db_id, 'path': path, 'fileName': filename, 'preProcessState': preProcessState})
 
     return response
 
 
 def update_readpre_path(db_id, path):
-    response = httpx.post(f'http://163.180.117.186:18088/api/predataset/post/updatereadprepath', json={'preDatasetId': db_id, 'path': path})
+    response = httpx.post(f'http://back.api:18088/api/predataset/post/updatereadprepath', json={'preDatasetId': db_id, 'path': path})
 
     return response
